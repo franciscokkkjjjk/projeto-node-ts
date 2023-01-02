d9df01c8a48f10942b0ca3ecc31c92a2ccc980da
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2023-01-02 17:03:29 -0300

    bobSponja

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,14 +1,18 @@
-import { Request, Response} from "express";
+import { Request, Response } from "express";
 
 export const home = (req: Request, res: Response) => {
-            
+    res.render('pages/page');
+    // res.send('teste');
 }
 export const cachorros = (req: Request, res: Response) => {
-
+    console.log(res.render);
+    // res.send('olá');
+    res.render('pages/page');
+    // res.render('pages/page');
 }
 export const gatos = (req: Request, res: Response) => {
 
 }
 export const peixes = (req: Request, res: Response) => {
 
-}
+}   
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
-import {Router, Request, Response} from "express";
+import Router from "express";
+import * as pageController from '../controllers/pageController';
+import * as searchController from '../controllers/searchController'; 
 
 const router = Router();
-
-router.get('/', (req: Request, res: Response)=>{
-    res.send('home');
-});
+router.get('/', pageController.home);   
+router.get('/cachorros', pageController.cachorros);   
+router.get('/gatos', pageController.gatos);   
+router.get('/peixes', pageController.peixes);   
+router.get('/buscar', searchController.search);   
 
 export default router;
